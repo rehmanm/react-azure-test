fa641118ddb621a61ef45a2f77f6988f4f408b62
Muhammad Abdul Rehman <muhammad.abdulrehman@gmail.com>
2023-02-07 09:47:14 -0500

    about page

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+
+function page() {
+  return (
+    <div>
+       This is about page
+    </div>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main >
-      Welcome to my site
+      Welcome to my site <br />
+      <Link href={"/about"}>About</Link>
     </main>
   )
 }
